56a77df71198a9caef424d04b00f3e41f261351a
Ikey Doherty <ikey.doherty@lispysnake.com>
2019-10-22 12:15:46 +0100

    assets: Add 2D world assets and clarify ownership
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ assets/raw/Inner.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Inner" tilewidth="16" tileheight="16" tilecount="1000" columns="40">
+ <image source="data/Inner.png" width="640" height="400"/>
+</tileset>
