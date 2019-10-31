e20087e88e3fdad433e24603fff3caea07b081db
Ikey Doherty <ikey.doherty@lispysnake.com>
2019-10-31 10:35:51 +0000

    texture: Add seriously messed up texture type
    We'll not be keeping this around in the long run, but we need to
    quickly get some kind of texture loady thingy in place to allow
    drawing textures.
    
    In time we'll have more optimised paths with DDS/KTX loading of
    HW-optimised textures. Next step: shaders.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ source/serpent/graphics/texture.d
@@ -0,0 +1,87 @@
+/*
+ * This file is part of serpent.
+ *
+ * Copyright © 2019 Lispy Snake, Ltd.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+module serpent.graphics.texture;
+
+import bindbc.sdl;
+import bindbc.sdl.image;
+import std.string : toStringz;
+import bindbc.bgfx;
+
+/**
+ * This is a very temporary type which will undergo many changes as
+ * the engine evolves.
+ */
+final class Texture
+{
+
+private:
+
+    SDL_Surface* surface = null;
+
+public:
+    bgfx_texture_handle_t _handle = cast(bgfx_texture_handle_t) 0;
+
+public:
+
+    /**
+     * Construct a new Texture from the given filename
+     *
+     * TODO: Make less hacky, try DDS load first, fallback to SDL_Image
+     * unoptimised path.
+     */
+    this(string filename)
+    {
+        surface = IMG_Load(toStringz(filename));
+        if (!surface)
+        {
+            return;
+        }
+
+        /* Convert the surface to something usable */
+        auto surface2 = SDL_ConvertSurfaceFormat(surface, SDL_PIXELFORMAT_RGBA32, 0);
+        SDL_FreeSurface(surface);
+        surface = surface2;
+
+        /* TODO: Optimise to the platform. */
+        auto fmt = bgfx_texture_format_t.BGFX_TEXTURE_FORMAT_RGBA8;
+        _handle = bgfx_create_texture_2d(cast(ushort) surface.w, cast(ushort) surface.h,
+                false, 1, fmt, 0, bgfx_make_ref(surface.pixels,
+                    surface.w * surface.h * surface.pitch));
+    }
+
+    ~this()
+    {
+        if (surface !is null)
+        {
+            SDL_FreeSurface(surface);
+        }
+    }
+
+    /**
+     * Return the underlying bgfx handle.
+     */
+    pure @property final bgfx_texture_handle_t handle() @nogc @safe nothrow
+    {
+        return _handle;
+    }
+}
