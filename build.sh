306042a967aa06fcecdf82ed151847b05011919c
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-01-18 20:56:33 +0000

    build: Start building in debug mode to stop hiding bugs
    Turns out release mode is optimising it's way through some genuine
    bugs with vectors. For now, let's stick to plain-as-day debug mode
    so we can catch all of our errors (before we get stinky code.)
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
 set -x
-dub build --parallel -c demo -b release --compiler=ldc2  --skip-registry=all
-strip ./serpent
+
+MODE="debug"
+
+if [[ "$MODE" == "release" ]]; then
+    dub build --parallel -c demo -b release --compiler=ldc2  --skip-registry=all
+    strip ./serpent
+else
+    dub build --parallel -c demo -b debug --compiler=ldc2 --skip-registry=all
+fi
 
