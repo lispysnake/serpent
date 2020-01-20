8797287e5a7eaf703b000c3cb352a1b0d85d1259
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-01-20 21:57:37 +0000

    assets/raw: Remove hack for the broken path support
    In lispysnake2d we didn't fully resolve relative paths (or even slightly)
    so let's remove that hack now before we start merging parsers in.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- assets/raw/Overworld.tsx
+++ assets/raw/Overworld.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.1" name="Overworld" tilewidth="16" tileheight="16" tilecount="1440" columns="40">
- <image source="data/Overworld.png" width="640" height="576"/>
+ <image source="Overworld.png" width="640" height="576"/>
  <tile id="16">
   <animation>
    <frame tileid="16" duration="135"/>
