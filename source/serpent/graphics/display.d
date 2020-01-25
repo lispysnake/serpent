442ddcf4117355e3b5b48536c647223cd27eefc5
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-01-25 21:37:47 +0000

    display: Stop being shy of Vulkan, let's embrace it!
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- source/serpent/graphics/display.d
+++ source/serpent/graphics/display.d
@@ -276,7 +276,7 @@ public:
         integrateWindowBgfx();
 
         /* TODO: Init on separate render thread */
-        bInit.type = bgfx_renderer_type_t.BGFX_RENDERER_TYPE_OPENGL;
+        bInit.type = bgfx_renderer_type_t.BGFX_RENDERER_TYPE_VULKAN;
         bgfx_init(&bInit);
         bgfx_reset(_width, _height, BGFX_RESET_VSYNC, bInit.resolution.format);
         updateDebug();
