3d183a49c47439adf4e8e11c6620d1e3c9f59a46
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-01-26 21:24:51 +0000

    sprite: Split the PosUVVertex into a new vertex module
    This may eventually grow into several modules but we'll see how it goes.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- source/serpent/graphics/sprite/renderer.d
+++ source/serpent/graphics/sprite/renderer.d
@@ -26,49 +26,16 @@ import bindbc.bgfx;
 import gfm.math;
 import std.stdint;
 
-import serpent.graphics.shader;
-import serpent.graphics.blend;
 import serpent.camera : WorldOrigin;
 import serpent.core.transform : TransformComponent;
+import serpent.graphics.shader;
+import serpent.graphics.blend;
+import serpent.graphics.vertex;
 
 public import serpent.graphics.sprite;
 public import serpent.core.entity;
 public import serpent.core.processor;
 public import serpent.core.view;
-
-/**
- * The PosUVVertex type simply contains coordinates for the position
- * of a Thing (3f), along with the texture coordinates (2f)
- *
- * It is currently only used for Sprite quads.
- */
-final struct PosUVVertex
-{
-
-    vec3f pos;
-    vec2f tex;
-
-    static bgfx_vertex_layout_t layout;
-
-    this(vec3f pos, vec2f tex)
-    {
-        this.pos = pos;
-        this.tex = tex;
-    }
-
-    static this()
-    {
-        bgfx_vertex_layout_begin(&layout, bgfx_renderer_type_t.BGFX_RENDERER_TYPE_NOOP);
-
-        /* Position */
-        bgfx_vertex_layout_add(&layout, bgfx_attrib_t.BGFX_ATTRIB_POSITION, 3,
-                bgfx_attrib_type_t.BGFX_ATTRIB_TYPE_FLOAT, false, false);
-        bgfx_vertex_layout_add(&layout, bgfx_attrib_t.BGFX_ATTRIB_TEXCOORD0,
-                2, bgfx_attrib_type_t.BGFX_ATTRIB_TYPE_FLOAT, false, false);
-
-        bgfx_vertex_layout_end(&layout);
-    }
-}
 
 /**
  * The SpriteRenderer will collect and draw all visible sprites within
--- source/serpent/graphics/vertex.d
+++ source/serpent/graphics/vertex.d
@@ -20,11 +20,41 @@
  * 3. This notice may not be removed or altered from any source distribution.
  */
 
-module serpent.graphics;
+module serpent.graphics.vertex;
 
-public import serpent.graphics.blend;
-public import serpent.graphics.display;
-public import serpent.graphics.shader;
+import bindbc.bgfx;
+import gfm.math;
 
-public import serpent.graphics.sprite;
-public import serpent.graphics.vertex;
+/**
+ * The PosUVVertex type simply contains coordinates for the position
+ * of a Thing (3f), along with the texture coordinates (2f)
+ *
+ * It is currently only used for Sprite quads.
+ */
+final struct PosUVVertex
+{
+
+    vec3f pos;
+    vec2f tex;
+
+    static bgfx_vertex_layout_t layout;
+
+    this(vec3f pos, vec2f tex)
+    {
+        this.pos = pos;
+        this.tex = tex;
+    }
+
+    static this()
+    {
+        bgfx_vertex_layout_begin(&layout, bgfx_renderer_type_t.BGFX_RENDERER_TYPE_NOOP);
+
+        /* Position */
+        bgfx_vertex_layout_add(&layout, bgfx_attrib_t.BGFX_ATTRIB_POSITION, 3,
+                bgfx_attrib_type_t.BGFX_ATTRIB_TYPE_FLOAT, false, false);
+        bgfx_vertex_layout_add(&layout, bgfx_attrib_t.BGFX_ATTRIB_TEXCOORD0,
+                2, bgfx_attrib_type_t.BGFX_ATTRIB_TYPE_FLOAT, false, false);
+
+        bgfx_vertex_layout_end(&layout);
+    }
+}
