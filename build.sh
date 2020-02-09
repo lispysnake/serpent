f6fbb28fb34650c4427f078836106f4cc40a9ea7
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-02-09 23:28:42 +0000

    build: Allow passing 'debug' and 'release' to script
    Instead of forever mangling the file provide an easier way for
    us to override the build configuration and remain consistent.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- build.sh
+++ build.sh
@@ -4,10 +4,17 @@ set -x
 
 MODE="debug"
 
+if [[ ! -z "$1" ]]; then
+    MODE="$1"
+fi
+
 if [[ "$MODE" == "release" ]]; then
     dub build --parallel -c demo -b release --compiler=ldc2  --skip-registry=all
     strip ./serpent
-else
+elif [[ "$MODE" == "debug" ]]; then
     dub build --parallel -c demo -b debug --compiler=ldc2 --skip-registry=all
+else
+    echo "Unknown build mode"
+    exit 1
 fi
 
