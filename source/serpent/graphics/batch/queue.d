c3c5f9476a75a987e3f50a8b3d17dbaef825b3a3
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-02-11 23:00:59 +0000

    batch: Finish the queue implementation
    The BatchQueue is an automatically growing structure that can be
    used to prepare draw operations on the CPU side before blitting them
    back to the GPU in a single copy.
    
    This allows us to allocate the right size buffer in the QuadBatch.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- source/serpent/graphics/batch/queue.d
+++ source/serpent/graphics/batch/queue.d
@@ -24,6 +24,9 @@ module serpent.graphics.batch.queue;
 
 public import std.container.array;
 
+import std.algorithm.mutation : copy;
+import core.stdc.string : memcpy;
+
 /**
  * A BatchQueue helps us to manage the CPU-size of vertex and index buffers
  * to prepare each set for uploading to the GPU.
@@ -40,6 +43,94 @@ private:
     ulong _size = 0;
     ulong _numIndices = 0;
     ulong _numVertices = 0;
+    ulong sizeVertices = 0;
+    ulong sizeIndices = 0;
+
+    ulong verticesIndex = 0;
+    ulong indicesIndex = 0;
+
+package:
+
+    /**
+     * Check if we need to resize the vertices
+     */
+    pragma(inline, true) final bool checkResizeVertices()
+    {
+        /* No need to resize, back we go */
+        if (verticesIndex + numVertices < sizeVertices)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Greedily resize the vertices if we can.
+     */
+    final bool performResizeVertices()
+    {
+        auto maxSize = _numVertices * _maxQuads;
+
+        /* Resize not possible */
+        if (verticesIndex + numVertices > maxSize - 1)
+        {
+            return false;
+        }
+
+        auto newSize = verticesIndex * 2;
+        if (newSize > maxSize)
+        {
+            newSize = maxSize;
+        }
+        sizeVertices = newSize;
+        vertices.reserve(sizeVertices);
+        vertices.length = sizeVertices;
+
+        return true;
+    }
+
+    /**
+     * Check if we need to resize indices
+     */
+    pragma(inline, true) final bool checkResizeIndices()
+    {
+        /* No need to resize, back we go */
+        if (indicesIndex + numIndices < sizeIndices)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Resize the indices
+     */
+    final bool performResizeIndices()
+    {
+        import std.stdio;
+
+        auto maxSize = _numIndices * _maxQuads;
+
+        /* Resize not possible */
+        if (indicesIndex + numIndices > maxSize - 1)
+        {
+            return false;
+        }
+
+        auto newSize = indicesIndex * 2;
+        if (newSize > maxSize)
+        {
+            newSize = maxSize;
+        }
+
+        sizeIndices = newSize;
+        indices.reserve(sizeIndices);
+        indices.length = sizeIndices;
+
+        return true;
+    }
 
 public:
 
@@ -53,6 +144,15 @@ public:
         this._maxQuads = maxQuads;
         this._numIndices = numIndices;
         this._numVertices = numVertices;
+
+        vertices = Array!V();
+        indices = Array!I();
+        vertices.reserve(minQuads * numVertices);
+        vertices.length = minQuads * numVertices;
+        indices.reserve(minQuads * numIndices);
+        indices.length = minQuads * numIndices;
+        sizeIndices = indices.length;
+        sizeVertices = vertices.length;
     }
 
     @disable this();
@@ -87,5 +187,79 @@ public:
     pragma(inline, true) pure @property final ulong numVertices() @safe @nogc nothrow
     {
         return _numVertices;
+    }
+
+    /**
+     * Push vertices into the vertex data buffer.
+     */
+    pragma(inline, true) final void pushVertices(V[] vertexData) @trusted
+    {
+        if (checkResizeVertices())
+        {
+            if (!performResizeVertices())
+            {
+                return;
+            }
+        }
+
+        vertexData.copy(vertices[verticesIndex .. $]);
+        verticesIndex += numVertices;
+    }
+
+    /**
+     * Push indices into the indices data buffer
+     */
+    pragma(inline, true) final void pushIndices(I[] indexData) @trusted
+    {
+        if (checkResizeIndices())
+        {
+            if (!performResizeIndices())
+            {
+                return;
+            }
+        }
+        indexData.copy(indices[indicesIndex .. $]);
+        indicesIndex += numIndices;
+
+    }
+
+    pragma(inline, true) final void reset() @trusted @nogc nothrow
+    {
+        indicesIndex = 0;
+        verticesIndex = 0;
+    }
+
+    /**
+     * Return the number of indices
+     */
+    pragma(inline, true) pure @property final ulong indicesCount() @safe @nogc nothrow
+    {
+        return indicesIndex;
+    }
+
+    /**
+     * Return the number of vertices
+     */
+    pragma(inline, true) pure @property final ulong verticesCount() @safe @nogc nothrow
+    {
+        return verticesIndex;
+    }
+
+    pragma(inline, true) final void copyIndices(I* target) @trusted
+    {
+        memcpy(cast(void*) target, cast(void*)&indices.front(), I.sizeof * indicesIndex);
+    }
+
+    pragma(inline, true) final void copyVertices(V* target) @trusted
+    {
+        memcpy(cast(void*) target, cast(void*)&vertices.front(), V.sizeof * verticesIndex);
+    }
+
+    /**
+     * Return true if the buffer is full
+     */
+    pragma(inline, true) pure @property final bool full() @safe @nogc nothrow
+    {
+        return indicesIndex + numIndices >= (_maxQuads * numIndices);
     }
 }
