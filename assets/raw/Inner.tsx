f24868f8685f3a0e9d54d569844866568d3c8b58
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-03-06 17:21:19 +0000

    Nuke unused assets
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

--- assets/raw/Inner.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Inner" tilewidth="16" tileheight="16" tilecount="1000" columns="40">
- <image source="Inner.png" width="640" height="400"/>
-</tileset>
