dbc4fa80aaa228301295488907117c54aba691fa
Ikey Doherty <ikey.doherty@lispysnake.com>
2020-03-07 00:31:55 +0000

    Integrate shader compilation into build process
    We don't *currently* consume the compiled shaders but we plan to. Right now
    we'll just automatically compile all in-tree shaders during build and spit
    out temporary files for them, per issue #13.
    
    Next stop is to import those files into the source and make them work.
    
    Signed-off-by: Ikey Doherty <ikey.doherty@lispysnake.com>

---
+++ scripts/prepare-shaders.d
@@ -0,0 +1,102 @@
+/*
+ * This file is part of serpent.
+ *
+ * Copyright © 2019-2020 Lispy Snake, Ltd.
+ *
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ *
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
+
+module shaderMain;
+
+import std.file;
+import std.process;
+import std.path;
+import std.stdio;
+
+void compileShader(string outputPath, string shaderPath, string varyingPath, string shaderLang, bool vertex)
+{
+    string command = "../serpent-support/runtime/bin/shaderc";
+
+    string outputFileName = outputPath.buildPath(shaderLang);
+    if (vertex)
+    {
+        outputFileName ~= ".vertex";
+    } else {
+        outputFileName ~= ".fragment";
+    }
+
+    /* TODO: Fix */
+    auto platform = "linux";
+    string[] args = [
+        command,
+        "-f", shaderPath,
+        "--type", vertex ? "vertex" : "fragment",
+        "-i", "../serpent-support/staging/bgfx/src",
+        "--platform", platform,
+        "-o", outputFileName,
+        "--varyingdef", varyingPath,
+    ];
+    /* Needed? */
+    if (platform != "linux" && shaderLang == "glsl") {
+        args ~= ["--profile", "glsl"];
+    } else if (shaderLang != "glsl")
+    {
+        args ~= ["--profile", shaderLang];
+    }
+
+    auto cmd = execute(args);
+    writeln(cmd.output);
+}
+
+void main()
+{
+    import std.stdio;
+
+    writeln("*** Preparing Serpent core shaders ***\n");
+
+    auto dir = environment.get("DUB_PACKAGE_DIR");
+    auto shaderDir = dir.buildPath("assets/shaders");
+    auto buildDir = dir.buildPath("built/shaders");
+
+    buildDir.mkdirRecurse();
+
+    foreach (item; dirEntries(shaderDir, SpanMode.shallow, false))
+    {
+        if (!item.isDir)
+        {
+            continue;
+        }
+        auto basenom = baseName(item);
+
+        writefln(" > %s", basenom);
+        auto fragment = shaderDir.buildPath(basenom, "fragment.sc");
+        auto vertex = shaderDir.buildPath(basenom, "vertex.sc");
+        auto varying = shaderDir.buildPath(basenom, "varying.def.sc");
+
+        assert(fragment.exists, "Missing fragment shader");
+        assert(vertex.exists, "Missing vertex shader");
+        assert(varying.exists, "Missing varying.def.sc");
+
+        auto outputPath = buildDir.buildPath(basenom);
+        outputPath.mkdirRecurse();
+
+        compileShader(outputPath, fragment, varying, "glsl", false);
+        compileShader(outputPath, vertex, varying, "glsl", true);
+        compileShader(outputPath, fragment, varying, "spirv", false);
+        compileShader(outputPath, vertex, varying, "spirv", true);
+    }
+}
